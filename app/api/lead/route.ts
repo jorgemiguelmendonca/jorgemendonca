415dfd4e324311b11466a13bb43303790c3c11f1
Clintin dos Sites <clintindossites@gmail.com>
2026-06-10 16:06:34 -0300

    .,,,

--- app/api/lead/route.ts
+++ app/api/lead/route.ts
@@ -4,6 +4,7 @@ export async function POST(req: Request) {
   const resend = new Resend(process.env.RESEND_API_KEY);
   try {
     const body = await req.json();
+    console.log(JSON.stringify(body, null, 2));
     const { formType, data } = body;
 
     let subject = "";
